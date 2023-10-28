160c5b7493ac3429712981fe2a5987cdd986f222
QuocHuy05 <vogiakhiem112002@gmail.com>
2023-10-28 12:48:05 +0700

    lop model Thu Thu

--- src/main/java/model/ThuThu.java
+++ src/main/java/model/ThuThu.java
@@ -4,10 +4,88 @@
  */
 package model;
 
+import java.util.Date;
+
 /**
  *
  * @author trand
  */
 public class ThuThu {
+    private String maThuThu;
+    private String hoTen;
+    private Date namSinh;
+    private String queQuan;
+    private String gioiTinh;
+    private String taiKhoan;
+    private String matKhau;
+
+    public ThuThu() {
+    }
+
+    public ThuThu(String maThuThu, String hoTen, Date namSinh, String queQuan, String gioiTinh, String taiKhoan, String matKhau) {
+        this.maThuThu = maThuThu;
+        this.hoTen = hoTen;
+        this.namSinh = namSinh;
+        this.queQuan = queQuan;
+        this.gioiTinh = gioiTinh;
+        this.taiKhoan = taiKhoan;
+        this.matKhau = matKhau;
+    }
+
+    public String getMaThuThu() {
+        return maThuThu;
+    }
+
+    public void setMaThuThu(String maThuThu) {
+        this.maThuThu = maThuThu;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public Date getNamSinh() {
+        return namSinh;
+    }
+
+    public void setNamSinh(Date namSinh) {
+        this.namSinh = namSinh;
+    }
+
+    public String getQueQuan() {
+        return queQuan;
+    }
+
+    public void setQueQuan(String queQuan) {
+        this.queQuan = queQuan;
+    }
+
+    public String getGioiTinh() {
+        return gioiTinh;
+    }
+
+    public void setGioiTinh(String gioiTinh) {
+        this.gioiTinh = gioiTinh;
+    }
+
+    public String getTaiKhoan() {
+        return taiKhoan;
+    }
+
+    public void setTaiKhoan(String taiKhoan) {
+        this.taiKhoan = taiKhoan;
+    }
+
+    public String getMatKhau() {
+        return matKhau;
+    }
+
+    public void setMatKhau(String matKhau) {
+        this.matKhau = matKhau;
+    }
     
 }

ad1e9e483acd12a025f079b70e7af13dd890dea3
QuocHuy05 <vogiakhiem112002@gmail.com>
2023-10-28 12:42:26 +0700

    lop model tac gia

--- src/main/java/model/TacGia.java
+++ src/main/java/model/TacGia.java
@@ -4,10 +4,58 @@
  */
 package model;
 
+import java.util.Date;
+
 /**
  *
  * @author trand
  */
 public class TacGia {
+    private String maTacGia;
+    private String hoTen;
+    private Date namSinh;
+    private String queQuan;
+
+    public TacGia() {
+    }
+
+    public TacGia(String maTacGia, String hoTen, Date namSinh, String queQuan) {
+        this.maTacGia = maTacGia;
+        this.hoTen = hoTen;
+        this.namSinh = namSinh;
+        this.queQuan = queQuan;
+    }
+
+    public String getMaTacGia() {
+        return maTacGia;
+    }
+
+    public void setMaTacGia(String maTacGia) {
+        this.maTacGia = maTacGia;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public Date getNamSinh() {
+        return namSinh;
+    }
+
+    public void setNamSinh(Date namSinh) {
+        this.namSinh = namSinh;
+    }
+
+    public String getQueQuan() {
+        return queQuan;
+    }
+
+    public void setQueQuan(String queQuan) {
+        this.queQuan = queQuan;
+    }
     
 }

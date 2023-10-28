cac42a955e082c8d148dc4885edfe09ef44191d6
QuocHuy05 <vogiakhiem112002@gmail.com>
2023-10-28 12:37:26 +0700

    lop model phieu Muon

--- src/main/java/model/PhieuMuon.java
+++ src/main/java/model/PhieuMuon.java
@@ -9,5 +9,41 @@ package model;
  * @author trand
  */
 public class PhieuMuon {
+    private String maPhieuMuon;
+    private String ThuThu;
+    private String maHocSinh;
+
+    public PhieuMuon() {
+    }
+
+    public PhieuMuon(String maPhieuMuon, String ThuThu, String maHocSinh) {
+        this.maPhieuMuon = maPhieuMuon;
+        this.ThuThu = ThuThu;
+        this.maHocSinh = maHocSinh;
+    }
+
+    public String getMaPhieuMuon() {
+        return maPhieuMuon;
+    }
+
+    public void setMaPhieuMuon(String maPhieuMuon) {
+        this.maPhieuMuon = maPhieuMuon;
+    }
+
+    public String getThuThu() {
+        return ThuThu;
+    }
+
+    public void setThuThu(String ThuThu) {
+        this.ThuThu = ThuThu;
+    }
+
+    public String getMaHocSinh() {
+        return maHocSinh;
+    }
+
+    public void setMaHocSinh(String maHocSinh) {
+        this.maHocSinh = maHocSinh;
+    }
     
 }

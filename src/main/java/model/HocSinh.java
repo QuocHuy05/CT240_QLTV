36dc391bc7f12be25116d29fce834311c866e52d
QuocHuy05 <vogiakhiem112002@gmail.com>
2023-10-28 11:54:51 +0700

    lop model hoc sinh

--- src/main/java/model/HocSinh.java
+++ src/main/java/model/HocSinh.java
@@ -4,10 +4,71 @@
  */
 package model;
 
+import java.util.Date;
+
 /**
  *
  * @author trand
  */
 public class HocSinh {
+    private String maHocSinh;
+    private String hoTen;
+    private String gioiTinh;
+    private Date namSinh;
+    private String queQuan;
+
+    public HocSinh() {
+    }
+
+    public HocSinh(String maHocSinh, String hoTen, String gioiTinh, Date namSinh, String queQuan) {
+        this.maHocSinh = maHocSinh;
+        this.hoTen = hoTen;
+        this.gioiTinh = gioiTinh;
+        this.namSinh = namSinh;
+        this.queQuan = queQuan;
+    }
+
+    public String getMaHocSinh() {
+        return maHocSinh;
+    }
+
+    public void setMaHocSinh(String maHocSinh) {
+        this.maHocSinh = maHocSinh;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public String getGioiTinh() {
+        return gioiTinh;
+    }
+
+    public void setGioiTinh(String gioiTinh) {
+        this.gioiTinh = gioiTinh;
+    }
+
+    public Date getNamSinh() {
+        return namSinh;
+    }
+
+    public void setNamSinh(Date namSinh) {
+        this.namSinh = namSinh;
+    }
+
+    public String getQueQuan() {
+        return queQuan;
+    }
+
+    public void setQueQuan(String queQuan) {
+        this.queQuan = queQuan;
+    }
+
+    
+    
     
 }
